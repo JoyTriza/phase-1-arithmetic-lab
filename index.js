1c732503d01a6b4d9ccd04016c7a06d12c4e89ea
JoyTriza <wangarijoytriza@gmail.com>
2022-04-09 00:30:43 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+let num1 = 61;
+let num2 = 1;
+let multiply = num1 * num2;
+console.log(multiply);
 
-// Write your code here
+
+let random = Math.floor(Math.random()*777)
+console.log (random);
+
+
+let num3 = 30;
+let num4 = 13;
+let mod = num3 % num4;
+console.log(mod);
+
+
+let max = Math.max(1,3,4,20,5,12)
+console.log(max);
